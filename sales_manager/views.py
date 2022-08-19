72950be0597f877ccbf938960ec20dadaf07055b
Mauricio Costa <mauriciodossantoscosta5@gmail.com>
2022-08-19 07:52:25 -0300

    added comment

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -9,5 +9,6 @@ def index():
 # função para gerar as faturas: fechamento da fatura 10 dias anstes da data da fatura - o sistema gera essa fatura.
 # função para mostar o saldo do cliente
 # visão do admin ficarã para o pai e ivanilson.
-# rotas de registro de oferta
-# 
+# rotas de registro de movimentação
+# rotas para ver a fatura do cliente - ivanilson - acessar pelo celular
+# cliente pode ver seu saldo ainda disponível, e faturas
